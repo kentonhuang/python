1d769052c2b23958b49f2dab233b50cd4bec60ba
kentonhuang <huangkenton@gmail.com>
2023-03-24 15:32:18 -0700

    day 39 3&4

--- flight-deals/main.py
+++ flight-deals/main.py
@@ -2,9 +2,14 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
 from pprint import pprint
+from datetime import datetime, timedelta
+from notification_manager import NotificationManager
+
+ORIGIN_CITY_IATA = "LON"
 
 data = DataManager()
 searcher = FlightSearch()
+notification_manager = NotificationManager()
 
 data.get_data()
 
@@ -23,3 +28,18 @@ if need_update:
     data.data = sheet_data
     data.update_iata_codes()
 
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = searcher.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.send_message(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        )
--- flight-deals/notification_manager.py
+++ flight-deals/notification_manager.py
@@ -1,3 +1,22 @@
+from dotenv import load_dotenv
+import os
+from twilio.rest import Client
+
+account_sid = os.getenv("TWILIO_SID")
+auth_token = os.getenv('TWILIO_AUTH')
+
+client = Client(account_sid, auth_token)
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        self.client = Client(account_sid, auth_token)
+
+    def send_message(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_='+18665167229',
+            to='+14154256678'
+        )
+
+        print(message.sid)
